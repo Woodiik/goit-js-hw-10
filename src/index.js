41722612b470f203c2dcd1c9fcb96974e27a409b
Woodiik <vovikvova04@gmail.com>
2023-02-12 17:30:40 +0200

    test

---
+++ src/index.js
@@ -0,0 +1,73 @@
+import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
+
+const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  searchBox: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+refs.searchBox.addEventListener(
+  'input',
+  debounce(onInputEvent, DEBOUNCE_DELAY)
+);
+
+function onInputEvent(e) {
+  if (e.target.value === '') {
+    return;
+  }
+  refs.countryList.innerHTML = '';
+  refs.countryInfo.innerHTML = '';
+
+  fetchCountries(e.target.value.trim())
+    .then(data => {
+      if (data.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+      if (data.length >= 2 && data.length <= 10) {
+        renderCountriesList(data);
+      }
+      if (data.length === 1) {
+        console.log(data);
+        renderCountryCard(data);
+      }
+    })
+    .catch(error => {
+      Notify.failure('Oops, there is no country with that name');
+    });
+}
+
+function renderCountriesList(data) {
+  data.map(({ flags, name }) => {
+    const markup = `<li class="country-list-item">
+          <svg class="country-svg" width="20" height="20">
+            <use href="${flags.svg}"></use>
+          </svg>
+        <span>${name.common}</span>
+      </li>`;
+    refs.countryList.insertAdjacentHTML('beforeend', markup);
+  });
+}
+
+function renderCountryCard(data) {
+  data.map(({ flags, name, population, languages, capital }) => {
+    const markup = `<div class="country">
+      <svg class="country-svg" width="20" height="20">
+        <use href="${flags.svg}"></use>
+      </svg>
+      <span class="country-name">${name.common}</span>
+    </div>
+    <div class="country-disc"><span class="country-disc-bold">Capital:</span> ${capital}</div>
+    <div class="country-disc"><span class="country-disc-bold">Population:</span> ${population}</div>
+    <div class="country-disc"><span class="country-disc-bold">Languages:</span> ${Object.values(
+      languages
+    )}</div>`;
+    refs.countryInfo.insertAdjacentHTML('beforeend', markup);
+  });
+}
